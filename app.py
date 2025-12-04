473cdc31f04e2a97c03c3df0c7471dfd5e937611
chursinaelizaveta2008 <chursina.elizaveta.2008@gmail.com>
2025-12-04 20:25:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 # Инициализация Flask приложения
 app = Flask(__name__)
@@ -16,9 +17,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Инициализация базы данных
 db = SQLAlchemy(app)
 
+# --- ИНИЦИАЛИЗАЦИЯ FLASK-LOGIN ---
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Пожалуйста, войдите в систему'
+login_manager.login_message_category = 'warning'
+
+# --- ЗАГРУЗЧИК ПОЛЬЗОВАТЕЛЯ ---
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 # --- МОДЕЛИ БАЗЫ ДАННЫХ ---
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     """Модель пользователя"""
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -66,7 +80,87 @@ class Note(db.Model):
     def __repr__(self):
         return f'<Note {self.title}>'
 
-# --- Маршруты будут здесь ---
+# --- МАРШРУТЫ ---
+
+@app.route('/')
+def index():
+    """Главная страница"""
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Регистрация пользователя"""
+    from flask import request
+    
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+        
+        # Валидация
+        if not all([username, email, password, confirm_password]):
+            flash('Все поля обязательны для заполнения', 'danger')
+            return redirect(url_for('register'))
+        
+        if password != confirm_password:
+            flash('Пароли не совпадают', 'danger')
+            return redirect(url_for('register'))
+        
+        # Проверка существования пользователя
+        if User.query.filter_by(username=username).first():
+            flash('Имя пользователя уже занято', 'danger')
+            return redirect(url_for('register'))
+        
+        if User.query.filter_by(email=email).first():
+            flash('Email уже зарегистрирован', 'danger')
+            return redirect(url_for('register'))
+        
+        # Создание пользователя
+        user = User(username=username, email=email)
+        user.set_password(password)
+        
+        db.session.add(user)
+        db.session.commit()
+        
+        flash('Регистрация успешна! Войдите в систему', 'success')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Вход в систему"""
+    from flask import request
+    
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.check_password(password):
+            login_user(user)
+            flash('Вход выполнен успешно!', 'success')
+            return redirect(url_for('dashboard'))
+        
+        flash('Неверное имя пользователя или пароль', 'danger')
+    
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    """Выход из системы"""
+    logout_user()
+    flash('Вы вышли из системы', 'info')
+    return redirect(url_for('index'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    """Личный кабинет пользователя"""
+    return "Дашборд будет здесь!"
 
 with app.app_context():
     db.create_all()
